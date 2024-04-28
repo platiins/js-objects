472db9b80487bb8435e013a95dcb9054f4358853
platiins <valerijaaivanovaa@gmail.com>
2024-04-28 18:18:37 +0300

    adding task 2 without last method

--- js/task2.js
+++ js/task2.js
@@ -9,21 +9,101 @@ Create an object that contains separately the numerator and the denominator of a
 
 */
 
-// const userNumerator = Number(
-//   prompt("please enter the numerator of a fraction:")
-// );
-// const userDenumerator = Number(
-//   prompt("please enter the denumerator of a fraction:")
-// );
+// const userNumerator1 = Number(prompt("enter first numerator:"));
+// const userDenumerator1 = Number(prompt("enter first denumerator:"));
 
-const fractionObject = {
-  numerator: 1,
-  denumerator: 4,
+// const userNumerator2 = Number(prompt("enter second numerator:"));
+// const userDenumerator2 = Number(prompt("enter second denumerator:"));
 
-  adding2fraction() {
-    let fraction = this.numerator / this.denumerator;
-    return Number(result);
+const userNumerator1 = 2;
+const userDenumerator1 = 4;
+
+const userNumerator2 = 1;
+const userDenumerator2 = 5;
+
+const firstFraction = {
+  numerator: userNumerator1,
+  denumerator: userDenumerator1,
+
+  fraction() {
+    let fractionResult = this.numerator / this.denumerator;
+    return Number(fractionResult);
   },
 };
 
-console.log(fractionObject.adding2fraction());
+console.warn(
+  `provided first numerator and first denumerator create ${firstFraction.fraction()} fraction`
+);
+
+const secondFraction = {
+  ...firstFraction,
+  numerator: userNumerator2,
+  denumerator: userDenumerator2,
+};
+console.warn(
+  `provided second numerator and second denumerator create ${secondFraction.fraction()} fraction`
+);
+
+// function for adding two fraction objects:
+
+function sum() {
+  let sumResult = secondFraction.fraction() + firstFraction.fraction();
+  return sumResult;
+}
+console.log(`sum result of these fractions is ${sum()}`);
+
+// function for subtracting two fraction objects:
+
+function subtracting() {
+  let subtractingResult;
+
+  if (firstFraction.fraction() === secondFraction.fraction()) {
+    subtractingResult = 0;
+  } else if (firstFraction.fraction() > secondFraction.fraction()) {
+    subtractingResult = firstFraction.fraction() - secondFraction.fraction();
+  } else {
+    subtractingResult = secondFraction.fraction() - firstFraction.fraction();
+  }
+
+  return subtractingResult;
+}
+
+console.log(`subtracting result of these fractions is ${subtracting()}`);
+
+// function for multiplying two fraction objects:
+
+function multiplying() {
+  let multiplyingResult = firstFraction.fraction() * secondFraction.fraction();
+  return multiplyingResult;
+}
+
+console.log(`multiplying result of these fractions is ${multiplying()}`);
+
+// function for dividing two fraction objects:
+
+function dividing() {
+  let dividingResult = firstFraction.fraction() / secondFraction.fraction();
+  return dividingResult;
+}
+
+console.log(`dividing result of these fractions is ${dividing()}`);
+
+// function for simplifying a fraction object:
+
+// function simplifying(a, b) {
+//   for (let i = 1; i <= Math.min(a, b); i++) {
+//     if (a % i === 0 && b % i === 0) {
+//       let divisor = i;
+//       let simplyfiedNumerator = a / divisor;
+
+//       console.log(simplyfiedNumerator);
+//       let simplyfiedDenumerator = b / divisor;
+
+//       console.log(simplyfiedDenumerator);
+
+//       return `${simplyfiedNumerator} / ${simplyfiedDenumerator}`;
+//     }
+//   }
+// }
+
+// console.log(simplifying(firstFraction.numerator, firstFraction.denumerator));
